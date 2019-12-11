f2d4d689a1b1ba17fa2deb6e628b962590109861
arik92 <arik92@gmail.com>
2019-12-11 16:51:44 +0200

    see if heroku deploys

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+let appPath = '';
+let appFileDir = '';
+
+const determineEnv = () => {
+  appPath =  process.env.NODE_ENV === 'dev' ? '/src/index.html' : '/dist/runTimez/index.html';
+  appFileDir =  process.env.NODE_ENV === 'dev' ? __dirname + '/src' : __dirname + '/dist/runTimez';
+  console.log('app path is %s \n and appFileDir is %s', appPath, appFileDir);
+};
+determineEnv();
+
 const forceSSL = function() {
     return function (req, res, next) {
       if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -15,12 +25,13 @@ const forceSSL = function() {
   
   // forceSSL middleware
   // app.use(forceSSL());
-app.use(express.static(__dirname + '/dist/runTimez'));
-// app.use(express.static(__dirname + '/src'));
-console.log('testing env? ', process.env.NODE_ENV);
-app.get('/*', function(req, res) {
-  const appPath = __dirname + process.env.NODE_ENV === 'dev' ? '/src/index.html' : '/dist/runTimez/index.html';
-    res.sendFile(path.join(appPath));
+// app.use(express.static());
+console.log('serving ', appFileDir);
+app.use(express.static(appFileDir));
+  console.log('app path ended up as ', appPath);
+app.get('/', function(req, res) {  
+    res.sendFile(path.join([__dirname, appPath]));
+    // res.sendFile(appPath);
   });
 // Start the app by listening on the default or Heroku port
 app.listen(process.env.PORT || 8080);
