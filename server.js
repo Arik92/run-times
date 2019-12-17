48d2e80631fb35e274ad330434011c80c102f8a7
arik92 <arik92@gmail.com>
2019-12-17 17:52:32 +0200

    heroku test

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const forceSSL = function() {
 app.use(express.static(__dirname + '/dist/runTimez'));
 // app.use(express.static(__dirname + '/src'));
 console.log('testing env? ', process.env.NODE_ENV);
-app.get('/*', function(req, res) {
-  const appPath = __dirname + process.env.NODE_ENV === 'dev' ? '/src/index.html' : '/dist/runTimez/index.html';
-    res.sendFile(path.join(appPath));
-  });
+// app.get('/*', function(req, res) {
+//   const appPath = __dirname + process.env.NODE_ENV === 'dev' ? '/src/index.html' : '/dist/runTimez/index.html';
+//     res.sendFile(path.join(appPath));
+//   });
 // Start the app by listening on the default or Heroku port
 app.listen(process.env.PORT || 8080);
